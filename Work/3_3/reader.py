5cbbb6e7935ce5ad41eb171e1e1b12279b79f60b
daniel-salmon <me@daniel-salmon.com>
2024-04-14 15:49:01 -0400

    Add solution to 3_3

---
+++ Work/3_3/reader.py
@@ -0,0 +1,29 @@
+# reader.py
+
+import csv
+
+def read_csv_as_dicts(filename, types):
+    '''
+    Read a CSV file into a list of dicts with column type conversion
+    '''
+    records = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            record = { name: func(val) for name, func, val in zip(headers, types, row) }
+            records.append(record)
+    return records
+
+        
+def read_csv_as_instances(filename, cls):
+    with open(filename, 'r') as f:
+        rows = csv.reader(f)
+        _ = next(rows)
+        records = []
+        for row in rows:
+            records.append(cls.from_row(row))
+    return records
+    
+
+
---
+++ Work/3_3/stock.py
@@ -0,0 +1,35 @@
+# stock.py
+
+import csv
+
+from decimal import Decimal
+
+class Stock:
+    types = (str, int, float)
+
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls.types, row)]
+        return cls(*values)
+
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
+
+
+class DStock(Stock):
+    types = (str, int, Decimal)
+
+
+def print_portfolio(portfolio):
+    print('%10s %10s %10s' % ('name', 'shares', 'price'))
+    print(f"{10 * '-'} {10 * '-'} {10 * '-'}")
+    for s in portfolio:
+        print('%10s %10d %10.2f' % (s.name, s.shares, s.price))

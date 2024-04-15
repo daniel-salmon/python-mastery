c669ca2eebf792c4107a8c492db9a66be400cffe
daniel-salmon <me@daniel-salmon.com>
2024-04-15 18:57:21 -0400

    Add 3_7

---
+++ Work/3_7/tableformat.py
@@ -0,0 +1,61 @@
+# tableformat.py
+
+from abc import ABC, abstractmethod
+
+
+class TableFormatter(ABC):
+    @abstractmethod
+    def headings(self, headers):
+        pass
+
+    @abstractmethod
+    def row(self, rowdata):
+        pass
+
+
+class TextTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(' '.join('%10s' % header for header in headers))
+        print(('-'*10 + ' ')*len(headers))
+
+    def row(self, rowdata):
+        print(' '.join('%10s' % r for r in rowdata))
+
+
+class CSVTableFormatter(TableFormatter):
+    def headings(self, headers):
+        print(','.join(headers))
+
+    def row(self, rowdata):
+        print(','.join(str(r) for r in rowdata))
+
+
+class HTMLTableFormatter(TableFormatter):
+    def headings(self, headers):
+        head = ' '.join('<th>%s</th>' % header for header in headers)
+        print(f'<tr> {head} </tr>')
+
+    def row(self, rowdata):
+        r = ' '.join('<td>%s</td>' % d for d in rowdata)
+        print(f'<tr> {r} </tr>')
+
+
+def create_formatter(fmt='text'):
+    if fmt == 'text':
+        return TextTableFormatter()
+    elif fmt == 'csv':
+        return CSVTableFormatter()
+    elif fmt == 'html':
+        return HTMLTableFormatter()
+    else:
+        raise NotImplementedError(f"No table formatter with format '{fmt}'")
+
+
+def print_table(records, fields, formatter):
+    if not isinstance(formatter, TableFormatter):
+        raise TypeError("Expected a TableFormatter")
+
+    formatter.headings(fields)
+    for record in records:
+        rowdata = [getattr(record, field, '') for field in fields]
+        formatter.row(rowdata)
